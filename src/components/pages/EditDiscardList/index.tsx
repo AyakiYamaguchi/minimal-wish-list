b9b5998bddca12ede3e54a946e46c12eac572488
AyakiYamaguchi <ikayayaki@gmail.com>
2021-02-20 14:18:15 +0900

    DiscardListの削除機能追加

--- src/components/pages/EditDiscardList/index.tsx
+++ src/components/pages/EditDiscardList/index.tsx
@@ -1,10 +1,15 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import Style from './EditDiscardList.module.scss';
 import { AuthContext } from '../../../store/Auth';
 import { useParams, useHistory } from 'react-router-dom';
-import { updateDiscardList, fetchDiscardListDetail } from '../../../apis/FirebaseDiscardList';
+import { updateDiscardList, fetchDiscardListDetail, deleteDiscardList } from '../../../apis/FirebaseDiscardList';
+import { deleteWishList } from '../../../apis/FirebaseWishList';
 import Header from '../../organisms/Header';
 import CommonListForm from '../../organisms/CommonListForm';
 import { DiscardList } from '../../../store/index';
+import DeleteButton from '../../atoms/DeleteButton';
+import ConfirmDeleteModal from '../../organisms/ConfirmDeleteModal';
+import { StoreContext, DELETE_WISH_LIST, DELETE_DISCARD_LIST } from '../../../store/index';
 
 type RouteParams = {
   id: string;
@@ -12,10 +17,12 @@ type RouteParams = {
 
 const EditDiscardList = () => {
   const { AuthState } = useContext(AuthContext);
+  const { setGlobalState } = useContext(StoreContext);
   const uid = AuthState.user.uid;
   const {id} = useParams<RouteParams>();
   const history = useHistory();
   const [discardList, setDiscardList] = useState<DiscardList>()
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
 
   const updateList = (values: any) => {
     updateDiscardList(uid,id,values.listName,values.iconId)
@@ -40,6 +47,26 @@ const EditDiscardList = () => {
         alert(error)
       })
   }
+  const deleteList = () => {
+    deleteDiscardList(uid, id)
+      .then(result=>{
+        setGlobalState({type: DELETE_DISCARD_LIST, payload: {discardListId: id}})
+        if (discardList?.wishListId){
+          deleteWishList(uid, discardList.wishListId)
+          .then(result=>{
+            if(discardList.wishListId){
+              setGlobalState({type: DELETE_WISH_LIST, payload: {wishListId: discardList.wishListId }})
+            }
+          }).catch(error=>{
+            alert(error)
+          })
+        }
+    }).catch(error=>{
+      alert(error)
+    })
+    history.push('/discard-lists')
+  }
+  
 
   useEffect(()=>{
     getDiscardList()
@@ -60,6 +87,18 @@ const EditDiscardList = () => {
           handleSubmit={updateList}
         />
       }
+      <div className={Style.delete_btn_wrapper}>
+        <DeleteButton 
+          btnText="このリストを削除する"
+          handleClick={()=>setModalIsOpen(true)}
+        />
+      </div>
+      <ConfirmDeleteModal 
+        isOpen={modalIsOpen}
+        confirmMessage={'Wishリストが存在する場合は、Wishリストも一緒に削除されますがよろしいですか？'}
+        handleDelete={deleteList}
+        handleCancel={()=>setModalIsOpen(false)}
+      />
     </div>
   )
 }
